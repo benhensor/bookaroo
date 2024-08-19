dbede2f27ea7af47fe94e554c05ee2d5d1459b6f
benhensor <benhensor@gmail.com>
2024-08-19 16:03:16 +0100

    vercel fix

--- server/config/database.mjs
+++ server/config/database.mjs
@@ -1,38 +1,48 @@
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config();
 
-// const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-// 		dialect: 'postgres',
-// 		dialectOptions: {
-// 			ssl: {
-// 				require: true,
-// 				rejectUnauthorized: false
-// 			}
-// 		},
-// 		pool: {
-// 			max: 5,
-// 			min: 0,
-// 			acquire: 30000,
-// 			idle: 10000
-// 		},
-// 	}
-// )
- 
-// export default sequelize
+console.log('Initializing Sequelize with the following configuration:', {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialectModule: pg,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
+export default sequelize;
 
 
 // Original code
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USERNAME,
-	process.env.DB_PASSWORD,
-	{
-		host: process.env.DB_HOST,
-		dialect: 'postgres',
-	}
-)
+// const sequelize = new Sequelize(
+// 	process.env.DB_NAME,
+// 	process.env.DB_USERNAME,
+// 	process.env.DB_PASSWORD,
+// 	{
+// 		host: process.env.DB_HOST,
+// 		dialect: 'postgres',
+// 	}
+// )
 
-export default sequelize
+// export default sequelize
