bd10ab32c4e09f3d6fef889d6c6eb35a20e1bb46
benhensor <benhensor@gmail.com>
2024-08-19 16:05:21 +0100

    sequelize sync fix

--- server/server.mjs
+++ server/server.mjs
@@ -53,7 +53,7 @@ app.use((req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000
-sequelize.sync().then(() => {
+sequelize.sync({ alterTable: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
